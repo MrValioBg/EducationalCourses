328eadfb1bd3c75b4f493c5972133901dbbcb5b1
MrValioBg <valentin.v1102@gmail.com>
2022-07-26 17:31:28 +0300

    Initial commit

---
+++ src/main/java/com/example/educationalcourses/EducationalCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.example.educationalcourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalCoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalCoursesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/educationalcourses/EducationalCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educationalcourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
