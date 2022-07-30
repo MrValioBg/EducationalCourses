058025167eea952056806289c7d203db8cc3d980
MrValioBg <valentin.v1102@gmail.com>
2022-07-30 13:38:41 +0300

    No longer using deprecated class

--- src/main/java/me/mrvaliobg/software/courses/configuration/SecurityConfig.java
+++ src/main/java/me/mrvaliobg/software/courses/configuration/SecurityConfig.java
@@ -4,21 +4,22 @@ package me.mrvaliobg.software.courses.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(AuthenticationManagerBuilder auth) {
+        UserDetails user = User
+                .withUsername("admin")
                 .password("$2a$12$aU51AHNqGqbubUQFt.dswuO3F3TglXmC4iJ/4ALi1I7lYMkT10nSe")
-                .roles("ADMIN");
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
